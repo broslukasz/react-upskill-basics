7c101e7085089eade13b4aaa5a2fea64f3789b03
Lukasz Bros <lbros@adnoc.ae>
2023-07-20 09:41:14 +0200

    some controls added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -11,6 +9,16 @@ import i18n from './i18n';
 import { useTranslation } from 'react-i18next';
 
 import Button from '@mui/material/Button';
+import { AppBar, Toolbar, MenuItem } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Grid from '@mui/material/Grid';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,6 +30,45 @@ function App() {
 
   return (
     <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" component="div">
+            Invoices
+          </Typography>
+
+          <MenuItem>
+            <Typography textAlign="center">New Invoice</Typography>
+          </MenuItem>
+        </Toolbar>
+      </AppBar>
+
+      <Grid mt={2} container spacing={2} p={2}>
+        <Grid item xs={6} container>
+          {/* Czy da się innaczej niż width 100% ? */}
+          <TextField sx={{ width: '100%' }} label="No." variant="standard" />
+          <Grid item container spacing={2} pt={2}>
+            <Grid item sm={6}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item sm={6}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker />
+              </LocalizationProvider>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6} container justifyContent="flex-end" alignItems="flex-start" gap={2}>
+          <Button className="mr-2" variant="outlined" startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+          <Button variant="contained" endIcon={<SendIcon />}>
+            Send
+          </Button>
+        </Grid>
+      </Grid>
+
       <div>
         <div>
           <Button variant="contained" onClick={() => changeLanguage('en')}>
@@ -32,21 +79,10 @@ function App() {
           </Button>
           <div>{t('Welcome to React')}</div>
         </div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }

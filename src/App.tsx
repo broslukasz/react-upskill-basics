63766eaafac0851ae09736669d559648bf596cfc
Lukasz Bros <lbros@adnoc.ae>
2023-09-05 09:06:12 +0200

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,29 @@
-import { useState } from 'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import './App.css';
-import i18n from './i18n';
-import { useTranslation } from 'react-i18next';
-
-import { AppBar, Toolbar, MenuItem } from '@mui/material';
-import Typography from '@mui/material/Typography';
 
 import InvoiceList from './components/InvoiceList';
 import Invoice from './components/Invoice';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: '', element: <InvoiceList /> },
+      { path: 'add', element: <Invoice /> },
+      { path: 'edit/:id', element: <Invoice /> },
+    ],
+  },
+]);
+
 function App() {
-  const [count, setCount] = useState(0);
-  const { t } = useTranslation();
-
-  const changeLanguage = (lng: string | undefined) => {
-    i18n.changeLanguage(lng);
-  };
-
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" component="div">
-            Invoices
-          </Typography>
-
-          <MenuItem>
-            <Typography textAlign="center">New Invoice</Typography>
-          </MenuItem>
-        </Toolbar>
-      </AppBar>
-
-      <InvoiceList />
-
-      <Invoice />
-
-      {/* <div>
-        <div>
-          <Button variant="contained" onClick={() => changeLanguage('en')}>
-            en
-          </Button>
-          <Button variant="contained" onClick={() => changeLanguage('pl')}>
-            pl
-          </Button>
-          <div>{t('Welcome to React')}</div>
-        </div>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div> */}
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,43 @@
+import { AppBar, Toolbar, MenuItem, Typography, Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Link, Outlet } from 'react-router-dom';
+import i18n from '../i18n';
+
+export default function Layout() {
+  const { t } = useTranslation();
+
+  const changeLanguage = (lng: string | undefined) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" component="div">
+            <Link to="" color="black">
+              Invoices
+            </Link>
+          </Typography>
+
+          <MenuItem>
+            <Typography textAlign="center">
+              <Link to="add">New Invoice</Link>
+            </Typography>
+          </MenuItem>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+
+      <div>
+        <Button variant="contained" onClick={() => changeLanguage('en')}>
+          en
+        </Button>
+        <Button variant="contained" onClick={() => changeLanguage('pl')}>
+          pl
+        </Button>
+        <div>{t('Welcome to React')}</div>
+      </div>
+    </>
+  );
+}

cde10072471508ad11ff528ba363807823b531e4
Lukasz Bros <lbros@adnoc.ae>
2024-01-08 08:45:47 +0100

    review changes

--- src/components/Invoice/EditInvoice.tsx
+++ src/components/Invoice/EditInvoice.tsx
@@ -1,7 +1,11 @@
 import { useParams } from 'react-router-dom';
 import InvoiceForm from './InvoiceForm';
 import { useInvoice } from './use-invoice';
-import { invoiceParamsSchema } from './params.schema';
+import { z } from 'zod';
+
+export const invoiceParamsSchema = z.object({
+  id: z.string(),
+});
 
 export default function EditInvoice() {
   const { id } = invoiceParamsSchema.parse(useParams());

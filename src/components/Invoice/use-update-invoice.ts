2fce7ac5d7c06a8cbade7c643d97668464d88946
Lukasz Bros <lbros@adnoc.ae>
2024-01-09 09:43:00 +0100

    body added

--- src/components/Invoice/use-update-invoice.ts
+++ src/components/Invoice/use-update-invoice.ts
@@ -3,7 +3,7 @@ import type { IInvoiceForm } from '../Models/InvoiceForm.interface';
 import { invoiceFormSchema } from '../Models/InvoiceForm.interface';
 
 const updateInvoice = (data: IInvoiceForm) =>
-  fetch(`/api/invoices/${data.id}`, { method: 'PUT' })
+  fetch(`/api/invoices/${data.id}`, { method: 'PUT', body: JSON.stringify(data) })
     .then((res) => {
       if (res.status > 399) {
         throw new Error(`Request failed with status ${res.status}`);

2601a96012d163af11997361245346656ff80e06
Lukasz Bros <lbros@adnoc.ae>
2024-01-12 12:21:28 +0100

    Snackbar disappear

--- src/components/Snackbars/SnackbarSuccess.tsx
+++ src/components/Snackbars/SnackbarSuccess.tsx
@@ -2,6 +2,7 @@ import { Snackbar } from '@mui/material';
 import type { AlertProps } from '@mui/material/Alert';
 import MuiAlert from '@mui/material/Alert';
 import * as React from 'react';
+import { NotificationContext } from '../../App';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -9,9 +10,18 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props,
 
 export const SnackbarSuccess = () => {
   const [open, setOpen] = React.useState(true);
+  const { notification, setNotification } = React.useContext(NotificationContext);
 
   return (
-    <Snackbar open={true} autoHideDuration={3000} message="Invoice Saved :)">
+    <Snackbar
+      open={open}
+      onClose={() => {
+        setOpen(false);
+        setNotification({ type: '', message: '' });
+      }}
+      autoHideDuration={3000}
+      message="Invoice Saved :)"
+    >
       <Alert severity="success" sx={{ width: '100%' }}>
         This is a success message!
       </Alert>

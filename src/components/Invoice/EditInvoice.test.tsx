b35bdde10a9ca21765b861b147e7363da4cd99b4
Lukasz Bros <lbros@adnoc.ae>
2024-02-07 12:15:40 +0100

    render with router created

--- src/components/Invoice/EditInvoice.test.tsx
+++ src/components/Invoice/EditInvoice.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { it, expect, describe } from 'vitest';
 import EditInvoice from './EditInvoice';
+import { renderWithRouter } from '../utils/render-with-router';
 
 describe('App', () => {
-  it('shoule work', async () => {
-    render(<EditInvoice />);
-    expect(true).toBe(true);
+  it('should work', async () => {
+    renderWithRouter(<EditInvoice />, { route: 'api/invoices/123' });
+    expect(screen.getByText(/no match/i)).not.toBeInTheDocument();
   });
 });

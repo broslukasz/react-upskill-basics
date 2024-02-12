12cbb47a9eef08e2287f761fee9f19630aaaf682
Lukasz Bros <lbros@adnoc.ae>
2024-02-12 14:23:08 +0100

    tests fired

--- src/components/Invoice/EditInvoice.test.tsx
+++ src/components/Invoice/EditInvoice.test.tsx
@@ -2,10 +2,16 @@ import { screen } from '@testing-library/react';
 import { it, expect, describe } from 'vitest';
 import EditInvoice from './EditInvoice';
 import { renderWithRouter } from '../utils/render-with-router';
+import { Route, Routes } from 'react-router-dom';
 
 describe('Edit Invoice', () => {
   it('should work', async () => {
-    renderWithRouter(<EditInvoice />, { route: 'api/invoices/123' });
-    expect(screen.getByText(/no match/i)).not.toBeInTheDocument();
+    renderWithRouter(
+      <Routes>
+        <Route element=<EditInvoice /> path="/invoice/:id" />
+      </Routes>,
+
+      { route: 'api/invoices/123' },
+    );
   });
 });
